b83dfe5bdd2010638499dfcabb95a891c2fcae49
KobengChantel <kobengchantel@gmail.com>
2024-04-03 08:30:20 +0200

    first commit

---
+++ My_Source_Code/lesson2/good.js
@@ -0,0 +1,12 @@
+let messages = [
+ "A change of environment can be a good thing!",
+ "You will make it!",
+ "Just run with the code!"
+];
+messages.forEach(message => console.log(message));
+let text = [
+ "A change of environment can be a good thing!",
+ "You will make it!",
+ "Just run with the code!"
+];
+text.forEach(message => console.log(message));
